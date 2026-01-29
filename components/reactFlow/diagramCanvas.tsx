88d022295dc2eeddd7545bdbdcf64321fcce9352
Aryan Mishra <aryan456.rehla@gmail.com>
2026-01-29 13:33:21 +0530

    changes from parser and canva both valid

--- components/reactFlow/diagramCanvas.tsx
+++ components/reactFlow/diagramCanvas.tsx
@@ -11,71 +11,14 @@ import ReactFlow, {
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { SystemNode } from './systemNode';
+import { useAppStore } from '@/lib/store';
 
 const nodeTypes = {
   system: SystemNode,
 };
 
-const initialNodes = [
-  { 
-    id: '1', 
-    type: 'system', 
-    position: { x: 250, y: 0 }, 
-    data: { label: 'User Client', nodeType: 'client', flag : 'offline' } 
-  },
-  { 
-    id: '2', 
-    type: 'system', 
-    position: { x: 250, y: 200 }, 
-    data: { label: 'API Gateway', nodeType: 'server', flag : 'offline' } 
-  },
-  { 
-    id: '3', 
-    type: 'system', 
-    position: { x: 250, y: 400 }, 
-    data: { label: 'Primary DB', nodeType: 'database', flag : 'online' } 
-  },
-  { 
-    id: '4', 
-    type: 'system', 
-    position: { x: 500, y: 400 }, 
-    data: { label: 'Redis Cache', nodeType: 'database', flag : 'offline' } 
-  },
-  {
-    id : '5',
-    type : 'system',
-    position : {x : 100, y : 200},
-    data : {label : 'Firewall', nodeType : 'firewall', flag : 'offline'}
-  }
-];
-
-const initialEdges = [
-  { 
-    id: 'e1-2', 
-    source: '1', 
-    target: '2', 
-    animated: true, 
-    label: 'Request' 
-  },
-  { 
-    id: 'e2-3', 
-    source: '2', 
-    target: '3', 
-    type: 'smoothstep',
-  },
-  { 
-    id: 'e2-4', 
-    source: '2', 
-    target: '4', 
-    type: 'smoothstep',
-    markerEnd: { type: MarkerType.ArrowClosed }, 
-    markerStart : {type : MarkerType.ArrowClosed }
-  },
-];
-
 export const BaseEditor = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const {nodes, edges, onNodesChange, onEdgesChange} = useAppStore()
 
   return (
     <div className="h-[100%] w-full bg-slate-50 dark:bg-slate-900">
--- lib/store.tsx
+++ lib/store.tsx
@@ -1,17 +1,46 @@
 import { create } from 'zustand'; 
-import { Node, Edge } from 'reactflow'; 
-
+import { Node, Edge, OnNodesChange, OnEdgesChange, applyEdgeChanges, applyNodeChanges } from 'reactflow'; 
 
 interface AppState {
   nodes: Node[];
   edges: Edge[];
   setGraph: (nodes: Node[], edges: Edge[]) => void;
+  onNodesChange : OnNodesChange;
+  onEdgesChange : OnEdgesChange;
 }
 
 
-export const useAppStore = create<AppState>((set) => ({
+export const useAppStore = create<AppState>((set, get) => ({
   nodes: [],
   edges: [],
 
-  setGraph: (nodes, edges) => set({ nodes, edges }),
+  setGraph: (newNodes, newEdges) => {
+    const currentNodes = get().nodes;
+
+    const mergedNodes = newNodes.map((newNode) => {
+      const existingNode = currentNodes.find((n) => n.id === newNode.id);
+
+      if(existingNode){
+        return {
+          ...newNode,
+          position : existingNode.position
+        };
+      }
+
+      return newNode;
+    });
+
+    set({nodes : mergedNodes, edges : newEdges})
+  };
+
+  onNodesChange : (changes) => {
+    set({
+      nodes : applyNodeChanges(changes, get().nodes),
+    })
+  },
+  onEdgesChange : (changes) => {
+    set({
+      edges : applyEdgeChanges(changes, get().edges),
+    })
+  }
 }));
