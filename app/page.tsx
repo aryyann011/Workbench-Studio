834f2906a5abb9616d28ed840242a81b1fd80a13
Aryan Mishra <aryan456.rehla@gmail.com>
2026-03-22 21:53:14 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,7 @@ export default function ResizableDemo() {
       });
 
       const data = await response.json()
+      console.log(data)
 
       if(data.code){
         setCode(data.code);
@@ -67,7 +68,7 @@ export default function ResizableDemo() {
             <CodeEditor onRun={handleRun}/>
           </div>
           {/* <div className="relative"> */}
-          <div className="w-full h-[64px]">
+          <div className="w-full h-16">
             <PromptBar prompt={prompt} setPrompt={setPrompt} onPromptRun={handlePromptRun} isloading={isloading}/>
           </div>
 
