6c593356b8f0632ffba2cc3c8136560f1ab2f2ae
Aryan Mishra <aryan456.rehla@gmail.com>
2026-03-29 22:56:16 +0530

    database mitigation

---
+++ prisma/migrations/20260329161737_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Workspace" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'Untitled Architecture',
+    "code" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
+);
