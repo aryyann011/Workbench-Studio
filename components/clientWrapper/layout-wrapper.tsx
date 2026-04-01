acf718486e58e62c9cfc056f5def954038a0b4e9
Aryan Mishra <aryan456.rehla@gmail.com>
2026-04-02 00:32:27 +0530

    logout error resolved

--- components/clientWrapper/layout-wrapper.tsx
+++ components/clientWrapper/layout-wrapper.tsx
@@ -27,13 +27,11 @@ export function LayoutWrapper({ children }: { children: React.ReactNode }) {
             <span className="text-lg font-medium text-muted-foreground"></span>
             <ModeToggle/>
             <div className="fixed flex items-center gap-8 right-0">
-              <SignOutButton>
-                <div className="flex items-center justify-center mr-2 cursor-pointer">
+              <SignOutButton redirectUrl="/">
+                <span className="flex items-center gap-2 px-3 py-2 rounded hover:bg-zinc-800 text-white cursor-pointer transition-colors">
                   <LogOut className="w-5 h-5"/>
-                  <button className="px-2 py-2 rounded text-white cursor-pointer">
-                    Logout
-                  </button>
-                </div>
+                  Logout
+                </span>
               </SignOutButton>
             </div>
           </div>
