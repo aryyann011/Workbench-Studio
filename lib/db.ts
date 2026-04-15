4df517777adb85b100f1e65c66859e2d0127dd43
Aryan Mishra <aryan456.rehla@gmail.com>
2026-04-16 03:51:32 +0530

    bug fix

--- lib/db.ts
+++ lib/db.ts
@@ -4,14 +4,19 @@ import { Pool } from 'pg'
 
 const connectionString = process.env.DATABASE_URL!
 
-const pool = new Pool({ connectionString })
-
+const pool = new Pool({ 
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, // <--- Bypasses your ISP's certificate block
+  }
+})
 const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+// We pass the adapter here because Prisma 7.6 strictly requires it
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
