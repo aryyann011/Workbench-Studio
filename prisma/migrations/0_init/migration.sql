376e51551482bb62a109fe7db0282cdf7f45f049
Aryan Mishra <aryan456.rehla@gmail.com>
2026-04-30 15:09:55 +0530

    prisma conflict resolved

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,12 +1,17 @@
+﻿-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
 CREATE TABLE "Workspace" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "name" TEXT NOT NULL DEFAULT 'Untitled Architecture',
     "code" TEXT NOT NULL,
-    "canvas_nodes" JSONB,
-    "canvas_edges" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "canvas_nodes" JSONB,
+    "canvas_edges" JSONB,
 
     CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
 );
+
