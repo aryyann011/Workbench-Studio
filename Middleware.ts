aa1b8b6f34f43dc3e45de19626131d6b5c5af91c
Aryan Mishra <aryan456.rehla@gmail.com>
2026-05-27 18:14:34 +0530

    minor fixes

--- Middleware.ts
+++ Middleware.ts
@@ -1,21 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// 1. Define which routes require a login. 
-// '(.*)' means EVERY single page in your app is locked down.
-const isProtectedRoute = createRouteMatcher(["/(.*)"]);
-
-const isPublicApiRoute = createRouteMatcher(["/api/(.*)"]);
-
+// Protect only the dashboard and its sub-paths
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard",
+  "/dashboard/(.*)"
+]);
 export default clerkMiddleware(async (auth, req) => {
-  if (isPublicApiRoute(req)) {
-    return;
-  }
-
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
-
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
